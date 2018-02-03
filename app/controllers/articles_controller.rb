87d66e7d7b52fe1bf10339b2b1fc5b88a317a96b
nnimish19 <nnimish19@gmail.com>
2018-02-03 18:12:47 -0500

    added validations: presence:true, min_length: 5

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,16 +8,24 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  # The reason why we added @article = Article.new in the ArticlesController is that otherwise
+  # @article would be nil in our view, and calling @article.errors.any? would throw an error.
   def new
+    @article = Article.new
   end
 
   def create
     # render plain: params[:article].inspect
-    # @article = Article.new(params[:article])
+    # @article = Article.new(params[:article])  #error: cannot save all parameters at once: strong_parameters
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article    #CALL to show action?
+    # @article.save
+    # redirect_to @article    #CALL to show action?
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
