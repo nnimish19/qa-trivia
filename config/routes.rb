3d3b38f3f0639ab4aca525eae8f0bd9ee1d0b13b
nnimish19 <nnimish19@gmail.com>
2018-02-03 14:48:13 -0500

    add new resource: articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index' #get 'welcome/index' tells Rails to map requests by browser to "http://localhost:3000/welcome/index" > to the welcome controller's index action
-  
+
+  resources :articles
+
   root 'welcome#index'  #root 'welcome#index' tells Rails to map requests to the "root of the application" to the "welcome controller's index action"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
