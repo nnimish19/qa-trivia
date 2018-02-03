3a8536eb8605366db9c6350199ecc544dc3ee49a
nnimish19 <nnimish19@gmail.com>
2018-02-03 14:27:35 -0500

    setting up home page: controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'welcome/index' #get 'welcome/index' tells Rails to map requests by browser to "http://localhost:3000/welcome/index" > to the welcome controller's index action
+  
+  root 'welcome#index'  #root 'welcome#index' tells Rails to map requests to the "root of the application" to the "welcome controller's index action"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
