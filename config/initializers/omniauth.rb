de917a42d9b41b7f9d284106e4c1a36896926440
nnimish19 <nnimish19@gmail.com>
2018-02-07 16:12:23 -0500

    temporary checkin

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  # provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '555214241505530', '72df4ddf7f6be221bc80967d558ee0ef'
 end
