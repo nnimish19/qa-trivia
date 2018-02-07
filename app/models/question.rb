7fd53bdd1b52d2d6e3fa9fa9a0fd050cdc9c5266
nnimish19 <nnimish19@gmail.com>
2018-02-07 13:25:50 -0500

    added model validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,11 @@
 class Question < ApplicationRecord
+  validates :statement, presence: true, length: { minimum: 5 }
+  validates :answer, presence: true
+
   belongs_to :user
-  has_many :tags
   has_many :responses
+  has_many :question_tags
+  has_many :tags, through: :question_tags
+
   
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,5 @@
 class Response < ApplicationRecord
+  validates :statement, presence: true
   belongs_to :user
   belongs_to :question
 end
